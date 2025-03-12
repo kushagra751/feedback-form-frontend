587ea9d4591c599773d0fe65999fc1f5beb1e965
Kushagra <patidarkush751@gmail.com>
2025-03-12 19:00:59 +0530

    Added Firebase setup

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_tT88C2Hgft1X3l9XDVstayzz0UVdB00",
+  authDomain: "feedback-form-4292a.firebaseapp.com",
+  projectId: "feedback-form-4292a",
+  storageBucket: "feedback-form-4292a.firebasestorage.app",
+  messagingSenderId: "764607130035",
+  appId: "1:764607130035:web:43079903233cc16019c3c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
